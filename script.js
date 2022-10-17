76db6f36fd4e1eef34b6e1c581efa4f0825058ae
rockypatel2613 <patelnilendra809@gmail.com>
2022-10-17 13:24:48 +0530

    add loop property

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const scroll = new LocomotiveScroll({
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
+    loop:true,
     autoplay: {
       delay: 2300,
       disableOnInteraction: false,
